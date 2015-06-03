e679389351ddd01d5f3640bc99c26ce3bf0dbb02
ganchau <gancchau@gmail.com>
2015-06-03 14:28:58 -0400

    trivia solved

--- solving-trivia/FISAppDelegate.m
+++ solving-trivia/FISAppDelegate.m
@@ -17,8 +17,91 @@
 {
 
     // write your solution here!
+    NSString *solution = @"";
     
-    return @"";
+    NSDictionary *statesAndCapitals = @{ @"Alabama" : @"Montgomery",
+                                         @"Alaska" : @"Juneau",
+                                         @"Arizona" : @"Phoenix",
+                                         @"Arkansas" : @"Little Rock",
+                                         @"California" : @"Sacramento",
+                                         @"Colorado" : @"Denver",
+                                         @"Connecticut" : @"Hartford",
+                                         @"Delaware" : @"Dover",
+                                         @"Florida" : @"Tallahassee",
+                                         @"Georgia" : @"Atlanta",
+                                         @"Hawaii" : @"Honolulu",
+                                         @"Idaho" : @"Boise",
+                                         @"Illinois" : @"Springfield",
+                                         @"Indiana" : @"Indianapolis",
+                                         @"Iowa" : @"Des Moines",
+                                         @"Kansas" : @"Topeka",
+                                         @"Kentucky" : @"Frankfort",
+                                         @"Louisiana" : @"Baton Rouge",
+                                         @"Maine" : @"Augusta",
+                                         @"Maryland" : @"Annapolis",
+                                         @"Massachusetts" : @"Boston",
+                                         @"Michigan" : @"Lansing",
+                                         @"Minnesota" : @"St. Paul",
+                                         @"Mississippi" : @"Jackson",
+                                         @"Missouri" : @"Jefferson City",
+                                         @"Montana" : @"Helena",
+                                         @"Nebraska" : @"Lincoln",
+                                         @"Nevada" : @"Carson City",
+                                         @"New Hampshire" : @"Concord",
+                                         @"New Jersey" : @"Trenton",
+                                         @"New Mexico" : @"Santa Fe",
+                                         @"New York" : @"Albany",
+                                         @"North Carolina" : @"Raleigh",
+                                         @"North Dakota" : @"Bismarck",
+                                         @"Ohio" : @"Columbus",
+                                         @"Oklahoma" : @"Oklahoma City",
+                                         @"Oregon" : @"Salem",
+                                         @"Pennsylvania" : @"Harrisburg",
+                                         @"Rhode Island" : @"Providence",
+                                         @"South Carolina" : @"Columbia",
+                                         @"South Dakota" : @"Pierre",
+                                         @"Tennessee" : @"Nashville",
+                                         @"Texas" : @"Austin",
+                                         @"Utah" : @"Salt Lake City",
+                                         @"Vermont" : @"Montpelier",
+                                         @"Virginia" : @"Richmond",
+                                         @"Washington" : @"Olympia",
+                                         @"West Virginia" : @"Charleston",
+                                         @"Wisconsin" : @"Madison",
+                                         @"Wyoming" : @"Cheyenne" };
+    
+    for (NSString *state in statesAndCapitals) {
+        
+        NSMutableArray *lettersInState = [@[] mutableCopy];
+        NSMutableArray *lettersInCapital = [@[] mutableCopy];
+        
+        for (NSUInteger i=0; i<state.length; i++) {
+            NSString *letter = [state substringWithRange:NSMakeRange(i, 1)];
+            if (![letter isEqualToString:@" "]) {
+                [lettersInState addObject:[letter lowercaseString]];
+            }
+        }
+        
+        NSString *capital = statesAndCapitals[state];
+        for (NSUInteger i=0; i<capital.length; i++) {
+            NSString *letter = [capital substringWithRange:NSMakeRange(i, 1)];
+            if (![letter isEqualToString:@" "]) {
+                [lettersInCapital addObject:[letter lowercaseString]];
+            }
+        }
+        
+        NSUInteger index = 0;
+        for (NSString *letter in lettersInState) {
+            if (![lettersInCapital containsObject:letter]) {
+                index++;
+                if (index == lettersInState.count) {
+                    solution = state;
+                }
+            }
+        }
+    }
+    
+    return solution;
 }
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
